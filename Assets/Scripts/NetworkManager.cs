0fc4637e807e5de0da9f91e6aaa23dddbbf82cc5
Hao <bearbbcjtc@gmail.com>
2022-04-13 16:11:03 -0400

    EscapeRoom

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+            PhotonNetwork.JoinRandomRoom();
+        else
+            PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+        Debug.Log("Created Room");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+    }
+}

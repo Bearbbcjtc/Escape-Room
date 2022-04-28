fe271582a841e4a13fb8f3cac3cfb7beaa180d34
Jed Wang <jcsaltfish@gmail.com>
2022-04-28 15:22:11 -0400

    Jed update 03

--- Assets/Scripts/EscapeManager.cs
+++ Assets/Scripts/EscapeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Photon.Pun;
 using Photon.Realtime;
@@ -8,14 +9,13 @@ using Photon.Realtime;
 public class EscapeManager : MonoBehaviour
 {
     public NetworkManager networkManager;
+    public Canvas canvas;
+    private bool showRole = false;
 
-    private enum PlayerRole
-    {
-        Mother,
-        GirlFriend,
-        Detective,
-        Classmate
-    }
+    private const int ROLE_MOTHER = 1;
+    private const int ROLE_DETECTIVE = 2;
+    private const int ROLE_GIRLFRIEND = 3;
+    private const int ROLE_CLASSMATE = 4;
 
     private int localRole = -1;
 
@@ -31,8 +31,30 @@ public class EscapeManager : MonoBehaviour
         if (localRole == -1)
         {
             if (networkManager.IsInARoom())
+            {
                 localRole = PhotonNetwork.LocalPlayer.ActorNumber;
+                showRole = true;
+            }
         }
-        Debug.Log(localRole);
+
+        if (showRole)
+        {
+            canvas.enabled = true;
+            string text = "You are ";
+            if (localRole == ROLE_MOTHER)
+                text += "Mother";
+            else if (localRole == ROLE_DETECTIVE)
+                text += "detective";
+            else if (localRole == ROLE_GIRLFRIEND)
+                text += "girlfriend";
+            else if (localRole == ROLE_CLASSMATE)
+                text += "classmate";
+
+            canvas.transform.GetChild(1).GetComponent<UnityEngine.UI.Text>().text = text;
+            if (OVRInput.Get(OVRInput.Button.One))
+                showRole = false;
+        }
+        else
+            canvas.enabled = false;
     }
 }

1ed5c4b6764b4fe388f44db0f40682b70972642f
Jed Wang <jcsaltfish@gmail.com>
2022-04-28 21:17:00 -0400

    Jed update 07

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,6 +7,7 @@ using Photon.Pun;
 public class Clock : MonoBehaviour
 {
     private float timeRemaining = -1.0f;
+    private float timeTotal = 600.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,16 @@ public class Clock : MonoBehaviour
             float time = timeRemaining - Time.deltaTime;
             GetComponent<PhotonView>().RPC("SyncTime", RpcTarget.All, time);
         }
+
+        Quaternion qHour = Quaternion.identity;
+        float angleHour = (1.0f - timeRemaining / timeTotal) * 90.0f;
+        qHour.eulerAngles = new Vector3(angleHour, 0, 0);
+        transform.GetChild(0).rotation = qHour;
+
+        Quaternion qMinute = Quaternion.identity;
+        float angleMinute = (timeTotal - timeRemaining) % (timeTotal / 3.0f) / (timeTotal / 3.0f) * 360.0f;
+        qMinute.eulerAngles = new Vector3(angleMinute, 0, 0);
+        transform.GetChild(1).rotation = qMinute;
     }
 
     public float GetTime()
@@ -31,7 +42,8 @@ public class Clock : MonoBehaviour
 
     public void SetTime(float time)
     {
-        GetComponent<PhotonView>().RPC("SyncTime", RpcTarget.All, time);
+        timeRemaining = time;
+        timeTotal = time;
     }
 
     [PunRPC]
--- Assets/Scripts/EscapeManager.cs
+++ Assets/Scripts/EscapeManager.cs
@@ -17,6 +17,13 @@ public class EscapeManager : MonoBehaviour
     private const int ROLE_GIRLFRIEND = 3;
     private const int ROLE_CLASSMATE = 4;
 
+    private const int ITEM_COFFEE = 1;
+    private const int ITEM_PROPERTY = 2;
+    private const int ITEM_BOTTLE = 3;
+    private const int ITEM_PHONE = 4;
+    private const int ITEM_DEBT = 5;
+    private const int ITEM_PHOTO = 6;
+
     private int localRole = -1;
 
     private int currentItemId = -1;
@@ -34,6 +41,15 @@ public class EscapeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeRemaining = clock.GetTime();
+        if (timeRemaining <= 0.0f && timeRemaining != -1.0f)
+        {
+            string text = "Time's up!";
+            canvas.transform.GetChild(1).GetComponent<Text>().text = text;
+            canvas.enabled = true;
+            return;
+        }
+
         if (localRole == -1)
         {
             if (networkManager.IsInARoom())
@@ -49,48 +65,75 @@ public class EscapeManager : MonoBehaviour
             }
         }
 
-        timeRemaining = clock.GetTime();
-        Debug.Log(timeRemaining);
-
         if (showInfo || showRole)
-            canvas.enabled = true;
+            canvas.gameObject.SetActive(true);
         else
-            canvas.enabled = false;
+            canvas.gameObject.SetActive(false);
 
         if (showRole)
         {
-            string text = "You are ";
+            string text = "";
             if (localRole == ROLE_MOTHER)
-                text += "Mother";
+            {
+                text += "Katerina\nRole Description: Although you appear to be a good mother to David, few people are aware that you are his stepmother. The main conflict you have with David is that is that he owns all of the family's property and has stated that he would not divide it with you. That's why you decided to murder him for the sake of the family's money. You poisoned everyone's coffee through the coffee machine, and then you put the antidote in the cake when you cut it in the kitchen. Everyone had the cake except David because you know he does not eat any desserts. That was how you conducted the murder successfully.\nSkills: Any clue you find, you can choose to hide it, and the other players will never know who does that. And you are the only player who can lie during the game.";
+            }
             else if (localRole == ROLE_DETECTIVE)
-                text += "detective";
+            {
+                text += "Parva\nRole Description: You are David's girlfriend. However, you two had recently been having a bad relationship because he wanted to break up with you while you refused. You two had a heated argument last night. You texted him, threatening to kill him if he broke up with you. You were just trying to scare him in this way. On the day of the murder, you offered the coffee to everyone using the coffee machine, and also delivered the cake to all the people except David, because you know he doesn't enjoy any dessert.\nSkills: What happened to David is unknown to you. You have the option of telling the facts you know, but you are not permitted to lie in the game.";
+            }
             else if (localRole == ROLE_GIRLFRIEND)
-                text += "girlfriend";
+            {
+                text += "Detective\nRole Description: You are David's friend, as well as a detective. You are the only one in the game who should not be suspected. Your job is to decide who the murderer is based on your own and other players' opinions. The decision you make will lead to the result if you guys can escape the room.\nSkills: You declare your role at the beginning of the game and make the final decision in the voting part.";
+            }
             else if (localRole == ROLE_CLASSMATE)
-                text += "classmate";
+            {
+                text += "Christian\nRole Description: You were David's high school classmate and are now a major pharmacy student. You two appear to be excellent friends, but few people are aware that you and David have a financial issue since you borrowed 30,000$ from David two years ago, and your contract with David states that the debt repayment deadline is approaching (2 days later), but you are unable to pay the money. You brought the cake on the day of the murder. You can tell David died of poisoning in the first place since your major is pharmacy.\nSkills: What happened to David is unknown to you. You have the option of telling the facts you know, but you are not permitted to lie in the game.";
+            }
 
-            canvas.transform.GetChild(1).GetComponent<UnityEngine.UI.Text>().text = text;
+            canvas.transform.GetChild(1).GetComponent<Text>().text = text;
             if (OVRInput.GetDown(OVRInput.Button.One))
                 showRole = false;
         }
 
         if (showInfo)
         {
-            if (OVRInput.GetDown(OVRInput.Button.One))
+            if (OVRInput.GetDown(OVRInput.Button.Two))
             {
                 // Found
                 string boardInfo = "\n";
-                if (currentItemId == 1)
-                    boardInfo += "Coffee maker";
+                if (currentItemId == ITEM_COFFEE)
+                {
+                    boardInfo += "Coffee machine";
+                }
+                else if (currentItemId == ITEM_PROPERTY)
+                {
+                    boardInfo += "Property Contract";
+                }
+                else if (currentItemId == ITEM_BOTTLE)
+                {
+                    boardInfo += "Antidote bottle";
+                }
+                else if (currentItemId == ITEM_PHONE)
+                {
+                    boardInfo += "David¡¯s smart phone";
+                }
+                else if (currentItemId == ITEM_DEBT)
+                {
+                    boardInfo += "Debt contract";
+                }
+                else if (currentItemId == ITEM_PHOTO)
+                {
+                    boardInfo += "David¡¯s childhood photo";
+                }
 
                 board.UpdateInfo(boardInfo);
 
                 showInfo = false;
             }
-            else if (localRole == ROLE_MOTHER && OVRInput.GetDown(OVRInput.Button.Three))
+            else if (localRole == ROLE_MOTHER && OVRInput.GetDown(OVRInput.Button.Four))
             {
                 // Hide
-                board.UpdateInfo("\nAn item has been hided by the murderer");
+                board.UpdateInfo("\nAn item has been hidden by the murderer");
 
                 showInfo = false;
             }
@@ -100,11 +143,34 @@ public class EscapeManager : MonoBehaviour
     public void ShowInfo(int itemId)
     {
         string info = "item";
-        if (itemId == 1)
-            info = "Coffee maker";
+        if (itemId == ITEM_COFFEE)
+        {
+            info = "Coffee machine\nCongratulations! You find some poison residues in the coffee machine, which indicates the reason for David's death.";
+        }
+        else if (itemId == ITEM_PROPERTY)
+        {
+            info = "Property Contract\nCongratulations! You find a property contract in the bookshelf. The information on it shows that David has the full ownership of all the family¡¯s property. The statement declares that David will not leave any of it to the person without blood relationship.";
+        }
+        else if (itemId == ITEM_BOTTLE)
+        {
+            info = "Antidote bottle\nCongratulations! You find an antidote bottle in the dustbin. The label on it shows that it can detoxify the poison found in the coffee machine.";
+        }
+        else if (itemId == ITEM_PHONE)
+        {
+            info = "David¡¯s smart phone\nCongratulations! You find a message in David¡¯s smart phone. The text shows:\nDavid: ¡°I¡¯m done with you¡±\nDavid: ¡°I¡¯m breaking up with you¡±\nDavid: ¡°Is what I¡¯m doing¡±\nParva: ¡°No way¡±\nParva: ¡°How dare you¡­ I will kill you if you do so!!¡±\nDavid: ¡°LET ME BREAK UP WITH YOU¡±\nParva: ¡°LET ME KILL YOU THEN¡±";
+        }
+        else if (itemId == ITEM_DEBT)
+        {
+            info = "Debt contract\nCongratulations! You find a debt contract in the drawer. It shows that Christian has a debt dispute with David, and the payment deadline is two days later.";
+        }
+        else if (itemId == ITEM_PHOTO)
+        {
+            info = "David¡¯s childhood photo\nCongratulations! You find a clue in a David¡¯s childhood photo. It shows that David does not eat any dessert. Katerina is his stepmother.";
+        }
 
         if (localRole == ROLE_MOTHER)
-            info += "\n\nYou can hide this item by pressing X";
+            info += "\n\nYou can hide this item by pressing Y";
+        info += "\nPress B to dismiss.";
 
         canvas.transform.GetChild(1).GetComponent<UnityEngine.UI.Text>().text = info;
         showInfo = true;
